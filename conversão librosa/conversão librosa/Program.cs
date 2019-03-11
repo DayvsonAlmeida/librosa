9c5c5f8faa6d0274ec923e6c6668f7595b1a478c
Dayvson Almeida <dayvsonwilkson@gmail.com>
2019-03-11 11:12:03 -0300

    Pad_reflect
    Implementação da função Pad_reflect

--- conversão librosa/conversão librosa/Program.cs
+++ conversão librosa/conversão librosa/Program.cs
@@ -10,20 +10,14 @@ namespace conversão_librosa{
         static void Main(string[] args){
             double []window;
             window = Spectrum.Get_window(10);
-            for(int i=0; i<10; i++)
+            for(int i=0; i<window.Length; i++)
                 Console.Write(window[i].ToString()+"| ");
             Console.Write("\nResizing...\n");
 
-            int size = 12;
             double[] new_window;
-            new_window = Util.Pad_center(window, size);
-            var y_frames = Util.Frame(window, 3, 2);
-            
-            for (int i = 0; i < y_frames.GetLength(0); i++){
-                for (int j = 0; j < y_frames.GetLength(1); j++) {
-                    Console.Write(y_frames[i,j].ToString()+" | ");
-                }
-                Console.WriteLine();
+            new_window = Util.Pad_reflect(window, 3);
+            for (int i = 0; i < new_window.Length; i++) {
+                Console.Write(new_window[i].ToString()+" | ");
             }
             Console.Read();
         }
@@ -59,6 +53,48 @@ namespace conversão_librosa{
             new_window = new double[size];
             int start = (new_window.Length - data.Length) / 2;
             Array.Copy(data, 0, new_window, start, data.Length);
+            return new_window;
+        }
+
+        /// <summary>
+        /// Realiza o padding do array de origem com o reflexo do mesmo.
+        /// </summary>
+        /// <param name="data">Source</param>
+        /// <param name="size">Tamanho do array de saída</param>
+        /// <returns></returns>
+        public static double[] Pad_reflect(double[] data, int size){
+            double[] new_window;
+            
+            //Copiando centralizada do array
+            int total_len = data.Length+2*size;
+            new_window = new double[total_len];
+            Array.Copy(data, 0, new_window, size, data.Length);
+
+            //Refletindo o array
+            int left = size-1;
+            int right = size+data.Length;
+            
+            int i = 1, j = data.Length - 2; //i: primeiro elemento a ser refletido para à esquerda; j: primeiro elemento a ser refletido à direita
+            int qtd = size;
+            int i_direction = 1;
+            int j_direction = -1;
+            while (qtd>0){
+                //Refletindo o item atual
+                new_window[left] = data[i];
+                new_window[right] = data[j];
+
+                //Alternando direção de reflexo
+                if (i + i_direction >= data.Length || i + i_direction < 0)
+                    i_direction *= -1;
+                if (j + j_direction >= data.Length || j + j_direction < 0)
+                    j_direction *= -1;
+
+                i += i_direction;
+                j += j_direction;
+                left--;
+                right++;
+                qtd--;
+            }
             return new_window;
         }
 
